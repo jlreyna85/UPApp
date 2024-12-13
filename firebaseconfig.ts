2c56e8741213c70fb2a2f8bfb57815f61b1c648f
Firg <33038499+jlreyna85@users.noreply.github.com>
2024-12-13 13:36:47 -0600

    Update firebaseconfig.ts

--- firebaseconfig.ts
+++ firebaseconfig.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
@@ -12,18 +12,3 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-=======
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage'; // Importa el servicio de Storage
-import { firebaseConfig } from './firebase-config'; // Asegúrate de que esta ruta sea correcta
-
-// Inicializar Firebase
-const app = initializeApp(firebaseConfig);
-
-// Servicios de Firebase
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
->>>>>>> 710010d346bc48bb2cae98df00d5a56031624116
